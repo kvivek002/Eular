43b3f694f69e6e4b591d6ac108ae77ac05113b10
Vivek Konzarkar <kvivek002@gmail.com>
2016-01-26 12:50:00 +0530

    Added code for Eular Problem Number 1

---
+++ Eular1 - MultipleOf3And5.py
@@ -0,0 +1,14 @@
+# Eular 1: Multiple of 3 and 5
+# xx
+
+def m35(n):
+    sum = 0
+    for i in range(1,n):
+        if i%3 == 0:
+            sum = sum + i
+            print(i, end=' ')
+        elif i%5 == 0:
+            sum = sum + i
+            print(i, end=' ')
+    print("\nSum of multiples of 3 or 5 below ",n," is ",sum)
+
